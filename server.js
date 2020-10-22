92b1e0c2d0ad25a9779e66980bb91c764a2772eb
Brandon Rorie <50763955+btror@users.noreply.github.com>
2020-10-22 17:36:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,43 +12,7 @@ app.listen(port, () => {
     console.log('test with local host 3000 ');
 });
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat Out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-        {
-            title: 'Entertainment',
-            budget: 55
-        },
-        {
-            title: 'Gas',
-            budget: 150
-        },
-        {
-            title: 'House Repairs',
-            budget: 300
-        },
-        {
-            title: 'Alimony',
-            budget: 500
-        },
-    ]
-};
-
-
  app.get('/manager', (req, res) => {
-    // const data = require('./budget');
-    // res.json(data);
     res.json(budget);
  });
 
